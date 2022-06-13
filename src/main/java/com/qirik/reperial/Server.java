5e050938c97fb3f10427d8f35cedfb764eca25dd
haidar <mhaidarhammam@gmail.com>
2022-06-13 07:53:18 +0700

    add brand and difficulty

--- src/main/java/com/qirik/reperial/Server.java
+++ src/main/java/com/qirik/reperial/Server.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Server {
+package com.qirik.reperial;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.world.Difficulty;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+        MinecraftServer server = MinecraftServer.init();
+        server.start("0.0.0.0", 25565);
+        server.setBrandName("Reperial");
+        server.setDifficulty(Difficulty.NORMAL);
+    }
 }
